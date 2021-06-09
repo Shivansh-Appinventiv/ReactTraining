d55d5766def8595ecd06d2550cd297d2cc194055
Shivansh-Appinventive <shivansh.srivastava@appinventiv.com>
2021-06-09 09:55:36 +0530

    resolved border issues in assignment 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import Assignment4 from "./modules/assignment4";
+//import Assignment4 from "./modules/assignment4";
 //import Home from "./modules/home";
 //import Assignment from "./modules/assignment1";
-//import Assignment2 from "./modules/assignment2";
+import Assignment2 from "./modules/assignment2";
 //import Assignment3 from "./modules/assignment3";
 
 class App extends React.Component {
@@ -10,10 +10,10 @@ class App extends React.Component {
     return (
       <div>
         {/* <Home />
-        <Assignment/>
+        <Assignment/> */}
         <Assignment2/>
-        <Assignment3/> */}
-        <Assignment4/>
+        {/* <Assignment3/>
+        <Assignment4/> */}
       </div>
     );
   }
